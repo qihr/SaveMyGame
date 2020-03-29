50085b90fe308014762c31a638ac632f2de360c1
qihr <1136055528@qq.com>
2020-03-29 23:59:33 +0800

    读取acf文件

--- Main/ModuleFile/LocalConfig.py
+++ Main/ModuleFile/LocalConfig.py
@@ -1,27 +1,46 @@
 import winreg
-import platform
 from steamfiles import acf
+import os
 import pytest
 
-testpath = 'F:/Game/Steam/steamapps/appmanifest_12120.acf'
+SteamHKey = r'SOFTWARE\WOW6432Node\Valve\Steam'
+GamesPath = ''
 
-@pytest.fixture(name='acf_data')
-def _acf_data():
-    with open(testpath, 'rt') as f:
-        yield f.read()
 
-def FindSteamLocation():
-    string = r'SOFTWARE\WOW6432Node\Valve\Steam'
-    handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, string)
+# find steam and acf file path
+def FindGamesLocation():
+    # find Steam install path
+    handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, SteamHKey)
     location, _type = winreg.QueryValueEx(handle, "InstallPath")
-    print('bit' + str(platform.architecture()))
-    print(location)
+
+    # set games info location path
+    global GamesPath
+    GamesPath = location + '\steamapps'
+    print(GamesPath)
 
 
-    with open(testpath, 'rt') as f:
+# load game info from acf file
+def LoadAppInfo(path):
+    print(path)
+    with open(path, errors='ignore') as f:
         line = f.read()
     data = acf.loads(line)
     print(data['AppState']['name'])
 
 
-FindSteamLocation()
+# init local game config,when first launch app
+def InitGamesInfoCsv():
+    allfiles = os.listdir(GamesPath)
+    acf_file_paths = []
+
+    for f in allfiles:
+        # Get all acf file
+        if os.path.splitext(f)[1] == '.acf':
+            acf_file_paths.append(f)
+
+    for file in acf_file_paths:
+        LoadAppInfo(GamesPath + '\\' + file)
+
+
+FindGamesLocation()
+InitGamesInfoCsv()
--- Main/__main__.py
+++ Main/__main__.py
@@ -2,9 +2,11 @@ import getopt
 import sys
 import LoadLocalFile
 import CopyFile
+import platform
 
 def main():
     print("Start Work")
+    print('System Bit' + str(platform.architecture()))
     GameList = LoadLocalFile.LoadGameCFG()
     CopyFile.CopyFile(GameList);
 
